8de1ebbf75365db45d9c9ebd79a2b41d7e1d07b7
Greg Cox <gcox@mozilla.com>
2022-10-27 07:00:21 +0000

    Allow peername (the IP field) to have substitutions

--- slapaclsuite/yaml_input_validator/administrative.py
+++ slapaclsuite/yaml_input_validator/administrative.py
@@ -10,9 +10,9 @@ class AdministrativeSectionValidator:
     ''' class that validates the 'administrative' section of the YAML config '''
     section = 'administrative'
 
-    def _field_name(self):
+    def _field_name(self, count=1):
         ''' What field does the calling function seek to validate? '''
-        calling_func_str = sys._getframe(1).f_code.co_name  # pylint: disable=protected-access
+        calling_func_str = sys._getframe(count).f_code.co_name  # pylint: disable=protected-access
         return self.validating_functions[calling_func_str]
 
     def _validate_name(self, value_in, verbose=False):
@@ -33,13 +33,13 @@ class AdministrativeSectionValidator:
             value_out = value_in
         return value_out
 
-    def _validate_dn_sub_key(self, value_in, verbose=False):
+    def __validate_sub_key(self, value_in, verbose=False):
         '''
             Validate a string for indicating runtime substitutions.
             Inputs: None or String+
             Returns: None or String+
         '''
-        my_field = self._field_name()
+        my_field = self._field_name(2)
         where_am_i = f'{self.section} / {my_field}'
         if verbose:
             print(f'## Preflighting {where_am_i}.')
@@ -53,13 +53,21 @@ class AdministrativeSectionValidator:
             value_out = value_in
         return value_out
 
-    def _validate_dn_subs(self, value_in, verbose=False):
+    def _validate_dn_sub_key(self, value_in, verbose=False):
+        ''' Simple wrapper '''
+        return self.__validate_sub_key(value_in, verbose)
+
+    def _validate_peername_sub_key(self, value_in, verbose=False):
+        ''' Simple wrapper '''
+        return self.__validate_sub_key(value_in, verbose)
+
+    def __validate_subs(self, value_in, verbose=False):
         '''
             Validate a dict of available substitutions
             Inputs: None or dict(String+: String+)
             Returns: dict(String+: String+)
         '''
-        my_field = self._field_name()
+        my_field = self._field_name(2)
         where_am_i = f'{self.section} / {my_field}'
         if verbose:
             print(f'## Preflighting {where_am_i}.')
@@ -76,6 +84,14 @@ class AdministrativeSectionValidator:
             value_out = value_in
         return value_out
 
+    def _validate_dn_subs(self, value_in, verbose=False):
+        ''' Simple wrapper '''
+        return self.__validate_subs(value_in, verbose)
+
+    def _validate_peername_subs(self, value_in, verbose=False):
+        ''' Simple wrapper '''
+        return self.__validate_subs(value_in, verbose)
+
     def __init__(self):
         '''
             build a dict of functions (above) that will validate pieces of the config
@@ -84,6 +100,8 @@ class AdministrativeSectionValidator:
             '_validate_name': 'name',
             '_validate_dn_sub_key': 'DN_substitutions_key',
             '_validate_dn_subs': 'DN_substitutions',
+            '_validate_peername_sub_key': 'peername_substitutions_key',
+            '_validate_peername_subs': 'peername_substitutions',
         }
         self.inputs = None
 
@@ -94,7 +112,10 @@ class AdministrativeSectionValidator:
             anyone of a certain class will do.
 
             Example:
-            Inputs: ('MAGIC:b', 'MAGIC:', {'a':'M', 'b':'N'}) -> 'N'
+            Input: 'MAGIC:b', when you have
+                   key = 'MAGIC:' and
+                   subs = {'a':'M', 'b':'N'}
+                   returns 'N'
         '''
         if self.inputs is None:
             raise RuntimeError(f'Can not perform DN subs before validation.')
@@ -105,6 +126,44 @@ class AdministrativeSectionValidator:
         subkey = self.inputs['DN_substitutions_key']
         subs = self.inputs['DN_substitutions']
 
+        if not subkey or not subs:
+            # "I don't want no subs"
+            return string_in
+        if not string_in.startswith(subkey):
+            # We're never going to match in order to do a substitution...
+            # So no sub happens: return the original.
+            return string_in
+
+        possible_key = string_in.replace(subkey, '', 1)
+        possible_value = subs.get(possible_key)
+        if possible_value is None:
+            return string_in
+        return possible_value
+
+    def possible_peername_substitutions(self, string_in):
+        '''
+            Substitute values from our YAML area so we can use stand-in peername/IPs.
+            This lets us templatize IPs in a test suite so 'stage' can use one peername.ip
+            and 'prod' another.
+
+            Example:
+            Inputs: 'IPFOR:stagebox', when you have
+                    key = 'IPFOR:' and
+                    subs = {'prodbox':'10.1.2.3', 'stagebox':'10.1.2.4'}
+                    returns: '10.1.2.4'
+        '''
+        if self.inputs is None:
+            raise RuntimeError(f'Can not perform peername subs before validation.')
+
+        if not isinstance(string_in, str):
+            raise ValueError(f'input-string must be a string')
+
+        subkey = self.inputs['peername_substitutions_key']
+        subs = self.inputs['peername_substitutions']
+
+        if not subkey or not subs:
+            # "subs is a key that can't get no love from me"
+            return string_in
         if not string_in.startswith(subkey):
             # We're never going to match in order to do a substitution...
             # So no sub happens: return the original.
@@ -167,5 +226,7 @@ class AdministrativeSectionValidator:
         config_out['name'] = self.inputs['name']
         config_out['DN_substitutions_key'] = self.inputs['DN_substitutions_key']
         config_out['DN_substitutions'] = self.inputs['DN_substitutions']
+        config_out['peername_substitutions_key'] = self.inputs['peername_substitutions_key']
+        config_out['peername_substitutions'] = self.inputs['peername_substitutions']
 
         return config_out
